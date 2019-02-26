9aac2ecdb216948975a3fbd1fa484ecf1b27dae5
Kristian <kristianuruplarsen@gmail.com>
2019-02-26 17:38:08 +0100

    initial commit

---
+++ favorite_functions.py
@@ -0,0 +1,15 @@
+from math import sqrt, ceil
+
+# (Kristian) My favorite function: 
+# The sieve of eratosthenes
+def sieve_of_eratosthenes(n: int) -> set:
+    """ Find all prime numbers less than n.
+    """
+    numberline = set(range(2, n))
+
+    for p in range(2, ceil(sqrt(n)) ):
+        for num in range(2*p, n, p):
+            numberline.discard(num)
+            
+    return numberline
+
